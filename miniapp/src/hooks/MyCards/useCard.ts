39169b09d26eaee9a50e2382c8e16ed73fb0c924
Sergey Knyazev <48366704+knyazevs@users.noreply.github.com>
2025-03-24 17:19:16 +0300

    Update useCard.ts

--- miniapp/src/hooks/MyCards/useCard.ts
+++ miniapp/src/hooks/MyCards/useCard.ts
@@ -93,7 +93,7 @@ const useCards = () => {
     if (selectedCardId) {
       const link = generatePath(ROUTES.CARD, { cardId: selectedCardId });
       navigator.clipboard
-        .writeText(`https://t.me/vizitko_bot?startapp=cardId_${selectedCardId}`)
+        .writeText(`https://t.me/card_bot?startapp=cardId_${selectedCardId}`)
         .then(() => {
           console.log(
             "Link copied to clipboard:",
