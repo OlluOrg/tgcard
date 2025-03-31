5bab8a2094c09cf07e55e2b7570126b08817f637
KirillNaRabote <kirillznaet@mail.ru>
2025-03-31 18:43:52 +0300

    Сделал открытие контекстного меню при клике на карточку

--- miniapp/src/components/CardList/CardList.tsx
+++ miniapp/src/components/CardList/CardList.tsx
@@ -1,11 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import styles from "../../pages/MyCardsPage/MyCardsPage.module.scss";
 import {TCard} from "../../types/types";
 import {Cell} from "@telegram-apps/telegram-ui";
-import {useAppDispatch, useAppSelector} from "../../hooks/hooks";
+import {useAppSelector} from "../../hooks/hooks";
 import useCard from "../../hooks/MyCards/useCard";
-import {readBusinessCards} from "../../store/apiThunks/businessCardThunks";
-import {useLocation} from "react-router-dom";
+import {ContextMenuItem, ContextMenuWrapperDiv} from "react-procedural-context-menu";
 
 const CardList = () => {
     const {cards, selectedCardId} = useAppSelector(state => state.myCards);
@@ -23,17 +22,29 @@ const CardList = () => {
                     <p className={styles.emptyStateTitle}>Тут пока пусто...</p>
                     <p className={styles.emptyStateSubtitle}>но вы можете добавить свою первую визитку</p>
                 </div>
-            ) : (sortCards.map((card: TCard, index: number) => (
-                <Cell
-                    key={card.businessCardId}
-                    subtitle={card.description}
-                    description={new Date(card.date).toLocaleDateString()}
-                    onClick={() => handleCardClick(card.businessCardId!)}
-                    hovered={card.businessCardId === selectedCardId}
-                >
-                    {card.title}
-                </Cell>
-            ))
+            ) : (sortCards.map((card: TCard, index: number) => {
+
+                    const menu: ContextMenuItem[] = [
+                        { text: "menuItem1", onClick: () => alert("menuItem1")},
+                        { text: "menuItem2", onClick: () => alert("menuItem2") }
+                    ];
+
+                    const menuCollection = { contextMenu: menu }
+
+                return (
+                    <ContextMenuWrapperDiv menus={menuCollection} >
+                    <Cell
+                        key={card.businessCardId}
+                        subtitle={card.description}
+                        description={new Date(card.date).toLocaleDateString()}
+                        onClick={() => handleCardClick(card.businessCardId!)}
+                        hovered={card.businessCardId === selectedCardId}
+                    >
+                        {card.title}
+                    </Cell>
+                </ContextMenuWrapperDiv>
+                )
+            })
         )}
         </div>
     );
