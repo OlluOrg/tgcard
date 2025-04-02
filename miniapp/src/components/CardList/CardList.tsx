22c71c6d39530de5a72b42302fc50dc8a5a63ed5
KirillNaRabote <kirillznaet@mail.ru>
2025-04-02 14:31:14 +0300

    Поменял библиотеку на для контекстного меню на radix-ui, исправил баг с закрытием контекстного меню

--- miniapp/src/components/CardList/CardList.tsx
+++ miniapp/src/components/CardList/CardList.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import styles from "../../pages/MyCardsPage/MyCardsPage.module.scss";
 import {TCard} from "../../types/types";
 import {Cell} from "@telegram-apps/telegram-ui";
-import {useAppDispatch, useAppSelector} from "../../hooks/hooks";
+import {useAppSelector} from "../../hooks/hooks";
 import useCard from "../../hooks/MyCards/useCard";
-import {ContextMenuItem, ContextMenuWrapperDiv} from "react-procedural-context-menu";
-import {selectCard} from "../../store/slices/myCardsSlice";
-import {ContextMenuPropsWithBindings} from "react-procedural-context-menu/dist/src/ContextMenu/ContextMenuWrapperDiv";
+import { ContextMenu } from "radix-ui";
 
 const CardList = () => {
     const {cards, selectedCardId} = useAppSelector(state => state.myCards);
@@ -17,47 +15,54 @@ const CardList = () => {
     };
     const sortCards = sortCardsByDate(cards);
 
-    const createContextMenu = (cardId: string): ContextMenuPropsWithBindings => {
-        const menu: ContextMenuItem[] = [
-            { text: "Открыть", onClick: () => handleCardClick(cardId)},
-            { text: "Редактировать", onClick: () => handleEdit(cardId)},
-            { text: "Настройки доступа", onClick: () => alert("Не готово") },
-            { text: "Архивировать", onClick: () => alert("Не готово") },
-            { text: "Поделиться", sub: [
-                    {text: "Скопировать ссылку", onClick: () => handleCopyLink(cardId)},
-                    {text: "Отправить ссылку", onClick: () => alert("Не готово")},
-                    {text: "Показать QR код", onClick: () => alert("Не готово")}
-                ]}
-        ];
-
-        return { contextMenu: menu }
-    }
-
     return (
-        <div className={styles.cardList}>
-            {cards.length === 0 ? (
-                <div className={styles.emptyState}>
-                    <p className={styles.emptyStateTitle}>Тут пока пусто...</p>
-                    <p className={styles.emptyStateSubtitle}>но вы можете добавить свою первую визитку</p>
-                </div>
-            ) : (sortCards.map((card: TCard, index: number) => {
-
-                return (
-                    <ContextMenuWrapperDiv menus={createContextMenu(card.businessCardId!)}>
-                    <Cell
-                        key={card.businessCardId}
-                        subtitle={card.description}
-                        description={new Date(card.date).toLocaleDateString()}
-                        onClick={() => handleCardClick(card.businessCardId!)}
-                        hovered={card.businessCardId === selectedCardId}
-                    >
-                        {card.title}
-                    </Cell>
-                </ContextMenuWrapperDiv>
-                )
-            })
-        )}
-        </div>
+        <>
+            <div className={styles.cardList}>
+                {cards.length === 0 ? (
+                    <div className={styles.emptyState}>
+                        <p className={styles.emptyStateTitle}>Тут пока пусто...</p>
+                        <p className={styles.emptyStateSubtitle}>но вы можете добавить свою первую визитку</p>
+                    </div>
+                ) : (sortCards.map((card: TCard, index: number) => {
+                        return (
+                            <ContextMenu.Root>
+                                <ContextMenu.Trigger>
+                                    <Cell
+                                        key={card.businessCardId}
+                                        subtitle={card.description}
+                                        description={new Date(card.date).toLocaleDateString()}
+                                        onClick={() => handleCardClick(card.businessCardId!)}
+                                        hovered={card.businessCardId === selectedCardId}
+                                    >
+                                        {card.title}
+                                    </Cell>
+                                </ContextMenu.Trigger>
+                                <ContextMenu.Portal>
+                                    <ContextMenu.Content>
+                                        <ContextMenu.Item onClick={() => handleCardClick(card.businessCardId!)}>Открыть</ContextMenu.Item>
+                                        <ContextMenu.Item onClick={() => handleEdit(card.businessCardId!)}>Редактировать</ContextMenu.Item>
+                                        <ContextMenu.Item onClick={() => alert("Не готово")}>Настройки доступа</ContextMenu.Item>
+                                        <ContextMenu.Item onClick={() => alert("Не готово")}>Архивировать</ContextMenu.Item>
+                                        <ContextMenu.Sub>
+                                            <ContextMenu.SubTrigger>
+                                                Поделиться
+                                            </ContextMenu.SubTrigger>
+                                            <ContextMenu.Portal>
+                                                <ContextMenu.SubContent>
+                                                    <ContextMenu.Item onClick={() => handleCopyLink(card.businessCardId!)}>Скопировать ссылку</ContextMenu.Item>
+                                                    <ContextMenu.Item onClick={() => alert("Не готово")}>Отправить ссылку</ContextMenu.Item>
+                                                    <ContextMenu.Item onClick={() => alert("Не готово")}>Показать QR код</ContextMenu.Item>
+                                                </ContextMenu.SubContent>
+                                            </ContextMenu.Portal>
+                                        </ContextMenu.Sub>
+                                    </ContextMenu.Content>
+                                </ContextMenu.Portal>
+                            </ContextMenu.Root>
+                        )
+                    })
+                )}
+            </div>
+        </>
     );
 };
 
