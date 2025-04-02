5426fa3e47ab913a57188d1c50c7329c216206d7
Alexei <moskvin.lyosha@gmail.com>
2025-04-02 12:38:10 +0300

    Создал структуру папок компонентов

---
+++ miniapp/src/components/SectionComponents/BlockLink/LinkModal/LinkModal.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import styles from "../../../../pages/CardPage/CardPage.module.scss";
+import {
+    ModalHeader
+} from "@telegram-apps/telegram-ui/dist/components/Overlays/Modal/components/ModalHeader/ModalHeader";
+import {setIsModalEditBlockLinkOpen} from "../../../../store/slices/modalsCardPageSlice";
+import {Button, Input, Modal} from "@telegram-apps/telegram-ui";
+import {useAppDispatch, useAppSelector} from "../../../../hooks/hooks";
+import useLinkSection from "../../../../hooks/CardPage/useLinkSection";
+import {setLinkBlockLinkInput, setNameBlockLinkInput} from "../../../../store/slices/linkSlice";
+
+const LinkModal = () => {
+    const dispatch = useAppDispatch();
+    const {isModalEditBlockLinkOpen, isEditBlock} = useAppSelector(state => state.modalsCardPage);
+
+    const {nameBlockLinkInput, linkBlockLinkInput, nameError, linkError} = useAppSelector(state => state.link)
+
+    const [isNameFocused, setIsNameFocused] = useState(true);
+    const [isLinkFocused, setIsLinkFocused] = useState(true);
+
+    const {
+        closeModalEditBlockLink,
+        handleEditBlockLink,
+        isBlockLinkValid,
+        handleAddBlockLink
+    } = useLinkSection();
+
+    return (
+        <Modal
+            className={styles.mdx}
+            style={{ zIndex: 1001}}
+            header={<ModalHeader>Создать ссылку</ModalHeader>}
+            open={isModalEditBlockLinkOpen}
+            onOpenChange={(open) => dispatch(setIsModalEditBlockLinkOpen(open))}
+        >
+            <div className={styles.inputGroup}>
+                <Input
+                    header="Название"
+                    autoFocus
+                    status={isNameFocused ? "default" : nameError ? "error" : "default"}
+                    placeholder="Название"
+                    value={nameBlockLinkInput}
+                    onFocus={() => setIsNameFocused(true)}
+                    onBlur={() => setIsNameFocused(false)}
+                    onChange={e => dispatch(setNameBlockLinkInput(e.target.value))}
+                />
+                {!isNameFocused && nameError && <div className={styles.errorMessage}>{nameError}</div>}
+            </div>
+            <div className={styles.inputGroup}>
+                <Input
+                    header="Ссылка"
+                    status={isLinkFocused ? "default" : nameError ? "error" : "default"}
+                    placeholder="Ссылка"
+                    value={linkBlockLinkInput}
+                    onFocus={() => setIsLinkFocused(true)}
+                    onBlur={() => setIsLinkFocused(false)}
+                    onChange={e => dispatch(setLinkBlockLinkInput(e.target.value))}
+                />
+                {!isLinkFocused && linkError && <div className={styles.errorMessage}>{linkError}</div>}
+            </div>
+            <div className={styles.modalAddBtns}>
+                <Button
+                    mode="outline"
+                    size="m"
+                    onClick={() => closeModalEditBlockLink()}
+                >
+                    Отмена
+                </Button>
+                <Button
+                    mode="filled"
+                    size="m"
+                    onClick={() => isEditBlock? handleEditBlockLink() : handleAddBlockLink()}
+                    disabled={!isBlockLinkValid()}
+                >
+                    {isEditBlock ? 'Сохранить' : 'Добавить'}
+                </Button>
+            </div>
+        </Modal>
+    );
+};
+
+export default LinkModal;
--- miniapp/src/pages/MyCardsPage/MyCardsPage.tsx
+++ miniapp/src/pages/MyCardsPage/MyCardsPage.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect} from 'react';
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import TopMenu from "../../components/TopMenu/TopMenu";
-import ModalAddCard from "../../components/ModalAddCard/ModalAddCard";
-import ModalDeleteCard from "../../components/ModalDeleteCard/ModalDeleteCard";
-import CardList from "../../components/CardList/CardList";
-import MyCardsBottomMenu from "../../components/MyCardsBottomMenu/MyCardsBottomMenu";
-import ModalSettingsCard from "../../components/ModalSettingsCard/ModalSettingsCard";
+import ModalAddCard from "../../components/CardComponents/ModalAddCard/ModalAddCard";
+import ModalDeleteCard from "../../components/CardComponents/ModalDeleteCard/ModalDeleteCard";
+import CardList from "../../components/CardComponents/CardList/CardList";
+import MyCardsBottomMenu from "../../components/BottomMenu/MyCardsBottomMenu/MyCardsBottomMenu";
+import ModalSettingsCard from "../../components/CardComponents/ModalSettingsCard/ModalSettingsCard";
 import {useAppDispatch, useAppSelector} from "../../hooks/hooks";
 import {readBusinessCards} from "../../store/apiThunks/businessCardThunks";
 import {useLocation} from "react-router-dom";
