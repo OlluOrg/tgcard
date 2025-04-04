05237f959b9ffcdff24526dec2873134aef42961
KirillNaRabote <kirillznaet@mail.ru>
2025-04-04 19:26:28 +0300

    сделал стили для контекстного меню, убрал ветвление на пункте Поделиться

--- miniapp/src/components/ContextMenu/ContextMenuWithCard/ContextMenuWithCard.tsx
+++ miniapp/src/components/ContextMenu/ContextMenuWithCard/ContextMenuWithCard.tsx
@@ -3,6 +3,7 @@ import {ContextMenu} from "radix-ui";
 import {TCard} from "../../../types/types";
 import useCard from "../../../hooks/MyCards/useCard";
 import Card from "../../CardComponents/Card/Card";
+import styles from './ConextMenuWithCard.module.scss'
 
 interface ContextMenuWithCardProps{
     card: TCard
@@ -17,24 +18,31 @@ const ContextMenuWithCard = (props: ContextMenuWithCardProps) => {
                 <Card card={props.card}/>
             </ContextMenu.Trigger>
             <ContextMenu.Portal>
-                <ContextMenu.Content>
-                    <ContextMenu.Item onClick={() => handleCardClick(props.card.businessCardId!)}>Открыть</ContextMenu.Item>
-                    <ContextMenu.Item onClick={() => handleEdit(props.card.businessCardId!)}>Редактировать</ContextMenu.Item>
-                    <ContextMenu.Item onClick={() => handleDelete(props.card.businessCardId!)}>Удалить</ContextMenu.Item>
-                    <ContextMenu.Item onClick={() => alert("Не готово")}>Настройки доступа</ContextMenu.Item>
-                    <ContextMenu.Item onClick={() => alert("Не готово")}>Архивировать</ContextMenu.Item>
-                    <ContextMenu.Sub>
-                        <ContextMenu.SubTrigger>
-                            Поделиться
-                        </ContextMenu.SubTrigger>
-                        <ContextMenu.Portal>
-                            <ContextMenu.SubContent>
-                                <ContextMenu.Item onClick={() => handleCopyLink(props.card.businessCardId!)}>Скопировать ссылку</ContextMenu.Item>
-                                <ContextMenu.Item onClick={() => alert("Не готово")}>Отправить ссылку</ContextMenu.Item>
-                                <ContextMenu.Item onClick={() => alert("Не готово")}>Показать QR код</ContextMenu.Item>
-                            </ContextMenu.SubContent>
-                        </ContextMenu.Portal>
-                    </ContextMenu.Sub>
+                <ContextMenu.Content className={styles.content}>
+                    <ContextMenu.Item className={styles.item}
+                                      onClick={() => handleCardClick(props.card.businessCardId!)}>Открыть
+                    </ContextMenu.Item>
+                    <ContextMenu.Item className={styles.item}
+                                      onClick={() => handleEdit(props.card.businessCardId!)}>Редактировать
+                    </ContextMenu.Item>
+                    <ContextMenu.Item className={styles.item}
+                                      onClick={() => handleDelete(props.card.businessCardId!)}>Удалить
+                    </ContextMenu.Item>
+                    <ContextMenu.Item className={styles.item}
+                                      onClick={() => alert("Не готово")}>Настройки доступа
+                    </ContextMenu.Item>
+                    <ContextMenu.Item className={styles.item}
+                                      onClick={() => alert("Не готово")}>Архивировать
+                    </ContextMenu.Item>
+                    <ContextMenu.Item className={styles.item}
+                                      onClick={() => handleCopyLink(props.card.businessCardId!)}>Скопировать ссылку
+                    </ContextMenu.Item>
+                    <ContextMenu.Item className={styles.item}
+                                      onClick={() => alert("Не готово")}>Отправить ссылку
+                    </ContextMenu.Item>
+                    <ContextMenu.Item className={styles.item}
+                                      onClick={() => alert("Не готово")}>Показать QR код
+                    </ContextMenu.Item>
                 </ContextMenu.Content>
             </ContextMenu.Portal>
         </ContextMenu.Root>
